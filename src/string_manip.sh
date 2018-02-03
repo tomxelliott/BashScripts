17a4c959b469b2d0fdc25ccfc8c853a3c2e6ed4e
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-02-03 11:21:17 +0000

    Update string_manip.sh

--- src/string_manip.sh
+++ src/string_manip.sh
@@ -17,3 +17,24 @@ sort -t: -k1,1 /dev/stdin
 
 # sort text file using first key (descending order)
 sort -t: -k1,1r /dev/stdin
+
+# sort numeric column in ascending order.
+sort -t: -k1n /dev/stdin
+
+# descending numeric order.
+sort -t: -k1nr /dev/stdin
+
+# (change flag depending on numeric or string data)
+# Change the table character to other symbols used.
+# eg for | use $'|' for tab delimiter
+# sort second column descending 
+sort -t$'\t' -k2 -rn /dev/stdin
+
+# sort second column ascending
+sort -t$'\t' -k2 -n /dev/stdin
+
+# sort and remove duplicates
+sort /dev/stdin | uniq
+
+# remove duplicates only if appear on back to back lines
+uniq -w5 /dev/stdin
