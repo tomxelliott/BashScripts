e9c1107735753c720cece90f2d0c5373e6bf1c35
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-02-03 10:53:16 +0000

    Update string_manip.sh

--- src/string_manip.sh
+++ src/string_manip.sh
@@ -12,5 +12,8 @@ sed 's/[A-Z]//g' /dev/stdin
 # Trim/squeeze any multiple spaces into single spaces
 tr -s " " < /dev/stdin 
 
-# sort text file using first key
+# sort text file using first key (ascending order)
 sort -t: -k1,1 /dev/stdin
+
+# sort text file using first key (descending order)
+sort -t: -k1,1r /dev/stdin
